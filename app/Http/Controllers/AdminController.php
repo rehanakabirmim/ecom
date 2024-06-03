750192d9fd07484a818dbaa0510867632cf10895
rehanakabirmim <rehanakabirmim@gmail.com>
2024-06-03 22:43:56 +0600

    Admin Login & Logout Customization part ok

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+    //end method
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }
+
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }
 }
