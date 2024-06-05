6b43c72175934a0df88ac841844838c02977fd6c
rehanakabirmim <rehanakabirmim@gmail.com>
2024-06-05 16:33:15 +0600

    Admin profile & image part almost done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -17,6 +18,15 @@ class AdminController extends Controller
     public function AdminLogin(){
         return view('admin.admin_login');
     }
+    //end method
+
+    public function AdminProfile(){
+        $id= Auth::user()->id;
+        $adminData=User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+    }
+      //end method
 
     public function AdminDestroy(Request $request): RedirectResponse
     {
