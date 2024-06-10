65c161132e2fb873d2e1f1a28caee6dc54731da0
rehanakabirmim <rehanakabirmim@gmail.com>
2024-06-11 00:56:17 +0600

    User Logout part done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,5 +42,18 @@ class UserController extends Controller
  
         
         return redirect()->back()->with($notification); 
-      }//end method  
+      }//end method 
+      
+      
+      public function UserDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+        //end method
 }
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [UserController::class, 'UserDashboard'])->name('dashboard');
     Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
 
+    Route::get('/user/logout', [UserController::class, 'UserDestroy'])->name('user.logout');
+
 
 });
 
