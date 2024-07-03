df753d6cda45dd5640bb39944669d0dac9f622a0
rehanakabirmim <rehanakabirmim@gmail.com>
2024-07-03 19:32:41 +0600

    show image using javascript for single & multiimage

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -1,6 +1,8 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <div class="page-content">
 
 				<!--breadcrumb-->
@@ -58,19 +60,34 @@
 
                             <div class="mb-3">
                                     <label for="inputProductTitle" class="form-label">Main Thambnail</label>
-                                    <input name="product_thambnail" class="form-control" type="file" id="formFile">
+                                    <input name="product_thambnail" class="form-control" type="file" id="formFile" onChange="mainThamUrl(this)">
+									<img src="" id="mainThmb" />
+
+									
                                 </div>
 
+								<div class="form-group mb-3">
+									<label for="inputProductTitle" class="form-label">Multiple Image</label>
+									<input class="form-control @error('multi_img')
+										is-invalid
+									@enderror" name="multi_img[]" type="file" id="multiImg"
+										multiple="" max="5">
+										@error('multi_img')
+											<span class="text-danger">{{$message}}</span>
+										@enderror
+									<div class="row my-3" id="preview_img"></div>
+								</div>
 
-							<div class="mb-3">
-								<label for="formFileMultiple" class="form-label">Multiple Image</label>
-								<input class="form-control" name="multi_img[]" type="file" id="formFileMultiple" multiple>
-							</div>
+								<!-- <div class="mb-3">
+									<label for="multiImg" class="form-label">Multiple Image</label>
+									<input class="form-control" name="multi_img[]" type="file" id="multiImg" multiple>
+									<div class="row" id="preview_img"></div>
 
- 
-							  
-                            </div>
-						   </div>
+			  					</div> -->
+					</div>
+			
+
+					</div>
 						   <div class="col-lg-4">
 							<div class="border border-3 p-4 rounded">
                               <div class="row g-3">
@@ -186,6 +203,54 @@
 
 			</div>
 
+
+<script >
+	function mainThamUrl(input){
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function(e){
+				$('#mainThmb').attr('src',e.target.result).width(80).height(80);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+</script>	
+
+<script>
+
+ // multi image preview
+ $(document).ready(function() {
+            $('#multiImg').on('change', function() { //on file input change
+                if (window.File && window.FileReader && window.FileList && window
+                    .Blob) //check File API supported browser
+                {
+                    var data = $(this)[0].files; //this file data
+
+                    $.each(data, function(index, file) { //loop though each file
+                        if (/(\.|\/)(gif|jpe?g|png|webp)$/i.test(file
+                            .type)) { //check supported file type
+                            var fRead = new FileReader(); //new filereader
+                            fRead.onload = (function(file) { //trigger function on successful read
+                                return function(e) {
+                                    var img = $('<img/>').addClass('thumb').attr('src',
+                                            e.target.result).width(100)
+                                        .height(80); //create image element
+                                    $('#preview_img').append(
+                                    img); //append image to output element
+                                };
+                            })(file);
+                            fRead.readAsDataURL(file); //URL representing the file's data.
+                        }
+                    });
+
+                } else {
+                    alert("Your browser doesn't support File API!"); //if File API is absent
+                }
+            });
+        });
+
+
+</script>
 	
  @endsection 
  
